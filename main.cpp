9aafd403d5e502ee9dc914ef922d538ba03f2208
MarttiWu <69631800+MarttiWu@users.noreply.github.com>
2020-12-24 22:29:15 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 //  main.cpp
 //  search
 //
-//  Created by WU,MENG-TING on 2020/7/1.
+//  Created by WU,MENG-TING on 2020/7/19.
 //  Copyright © 2020 WU,MENG-TING. All rights reserved.
 //
 
@@ -13,21 +13,25 @@
 
 using namespace std;
 
-void prompt(const int Algo,const int runs,const int generations,const int bits);
+void prompt(const int Algo,const int runs,const int iterations);
 
 int main(int argc, const char * argv[]) {
     const string algo = argv[1];
     const int runs = stoi(argv[2]);
-    const int generations = stoi(argv[3]);
-    string filename = argv[4];
+    const int iterations = stoi(argv[3]);
+    //const int bits = stoi(argv[4]);
+    
+    string filename= argv[4];
     cout<<"filename: "<<filename<<endl;
+    
     const int population = stoi(argv[5]);
-    const double alpha = stod(argv[6]);
-    const double beta = stod(argv[7]);
-    const double rho = stod(argv[8]);
+    const double alpha = stoi(argv[6]);
+    const double beta = stoi(argv[7]);
+    const double rho = stoi(argv[8]);
+    
     if (algo=="ac"){
-        prompt(1, runs, generations, bits);
-        execute(1, runs, generations, bits, filename,temperature, alpha);
+        prompt(1, runs, iterations);
+        execute(1, runs, iterations, filename, population, alpha, beta, rho);
     }
     else{
         cerr<<"No such algorithm!"<<endl;
@@ -35,7 +39,9 @@ int main(int argc, const char * argv[]) {
     return 0;
 }
 
-void prompt(const int Algo,const int runs,const int generations,const int bits){
+
+void prompt(const int Algo,const int runs,const int iterations){
+    cout<<"Problem: TSP51"<<endl;
     cout<<"----------------------------------------"<<endl;
     switch (Algo) {
         case 1:
@@ -45,6 +51,6 @@ void prompt(const int Algo,const int runs,const int generations,const int bits){
             break;
     }
     cout<<"runs: "<<runs<<endl;
-    cout<<"generations: "<<generations<<endl;
+    cout<<"iterations: "<<iterations<<endl;
     cout<<"----------------------------------------"<<endl;
 }
