1eed14f5dddd0e7d2d53627dd25374dd0ad06fd4
MarttiWu <69631800+MarttiWu@users.noreply.github.com>
2020-12-24 22:29:12 +0800

    Update execute.hpp

--- execute.hpp
+++ execute.hpp
@@ -2,7 +2,7 @@
 //  execute.hpp
 //  search
 //
-//  Created by WU,MENG-TING on 2020/7/1.
+//  Created by WU,MENG-TING on 2020/7/8.
 //  Copyright © 2020 WU,MENG-TING. All rights reserved.
 //
 
@@ -12,13 +12,10 @@
 #include <stdio.h>
 #include <iostream>
 #include <ctime>
-#include <fstream>
 #include "AC.hpp"
+
 using namespace std;
 
-const int block = 10;
-
-void execute(const int Algo,const int runs,const int generations,const string filename,const int population,const double alpha,const double beta,const double rho);
+void execute(const int Algo,const int runs,const int iterations,const string filename,const int population,const double alpha,const double beta, const double rho);
 
 #endif /* execute_hpp */
-
