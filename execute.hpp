c0c186137f383d75b99921dfa5984770f4b96ff5
MarttiWu <69631800+MarttiWu@users.noreply.github.com>
2021-01-03 13:20:05 +0800

    Update execute.hpp

--- execute.hpp
+++ execute.hpp
@@ -16,6 +16,6 @@
 
 using namespace std;
 
-void execute(const int Algo,const int runs,const int iterations,const string filename,const int population,const double alpha,const double beta, const double rho);
+void execute(const int Algo,const int runs,const int iterations,const string filename,const int population,const double alpha,const double beta, const double rho, const int two_opt);
 
 #endif /* execute_hpp */
