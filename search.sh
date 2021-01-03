d26074f5a4da1c2c23e208cf78684b0e36a0c192
MarttiWu <69631800+MarttiWu@users.noreply.github.com>
2021-01-03 13:19:51 +0800

    Update search.sh

--- search.sh
+++ search.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 set -x
-#./main [algo] [runs] [gen] [filename] [population] [alpha] [beta] [rho]
-./main ac 30 1250 "" 30 1 5 0.8
+#./main [algo] [runs] [gen] [filename] [population] [alpha] [beta] [rho] [2opt]
+./main ac 30 1250 "eil51.txt" 30 1 5 0.8 0
+./main ac 30 1250 "eil51.txt" 30 1 5 0.8 2
+./main ac 30 1250 "eil101.txt" 30 1 5 0.8 0
+./main ac 30 1250 "eil101.txt" 30 1 5 0.8 2
 set +x
